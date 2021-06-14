7cf9cec8131504d9298f5237da4353f7350e617c
emersonpessoa01 <eps011108@gmail.com>
2021-06-13 21:43:04 -0300

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext";
 import { Login } from "./pages/login";
 import { Dashboard } from "./pages/Dashboard";
-import  history  from "./services/history";
+import history from "./services/history";
+import { Context } from "./Context/AuthContext";
 
 function App() {
+  const CustomRoute = ({ isPrivate, ...rest }) => {
+    const { authenticated } = useContext(Context);
+    if (isPrivate && !authenticated) {
+      return <Redirect to="/" />;
+    }
+    return <Route {...rest} />;
+  };
+
   return (
     <div>
       {/* Todas s rotas vão está dentro desse AuthProvider e todas a rotas estão recebendo o context*/}
       <AuthProvider>
         <Router history={history}>
           <Switch>
-            <Route exact path="/" component={Login} />
-            <Route path="/Dashboard" component={Dashboard} />
+            <CustomRoute exact path="/" component={Login} />
+            <CustomRoute isPrivate path="/Dashboard" component={Dashboard} />
           </Switch>
         </Router>
       </AuthProvider>
